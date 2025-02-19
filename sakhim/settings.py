8482a6d445dcad41a31509f601181eafb1eaa4ff
DORN Mesa <dornmesa8@gmail.com>
2025-02-19 21:53:43 +0700

    Cristaino Ronaldo Is The Goat

--- sakhim/settings.py
+++ sakhim/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sakhim2025@gmail.com"  # Replace with your Gmail
-EMAIL_HOST_PASSWORD = "Cambodiakh001"  # Use the App Password
+EMAIL_HOST_USER = ""  # Replace with your Gmail
+EMAIL_HOST_PASSWORD = ""  # Use the App Password
